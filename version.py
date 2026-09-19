01d10ea870678fab28160de14ae587b38597f3c4
WilsonNous <107373913+WilsonNous@users.noreply.github.com>
2026-09-19 14:59:07 -0300

    Add files via upload

--- version.py
+++ version.py
@@ -1,2 +1,2 @@
-APP_VERSION = "3.28.42"
-APP_RELEASE = "Cliente resolvido · chamados clicáveis · operação compacta"
+APP_VERSION = "3.28.43"
+APP_RELEASE = "Verocheque completo · trava de atuação prévia · continuidade segura"
