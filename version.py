e3ddd63fd67b3dec78d2590c38aac195842034d4
WilsonNous <107373913+WilsonNous@users.noreply.github.com>
2026-09-20 17:06:46 -0300

    Add files via upload

--- version.py
+++ version.py
@@ -1,2 +1,2 @@
-APP_VERSION = "3.28.45"
-APP_RELEASE = "Barreira global de primeira atuação · Policard CNPJ+EC · continuidade por estado Redmine"
+APP_VERSION = "3.28.46"
+APP_RELEASE = "Multi-EC TICKET · e-mails completos · follow-up HTML formatado"
